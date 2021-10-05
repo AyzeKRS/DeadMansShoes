146c7ce80ff62692ea5879231e668d5e1ce25b25
Joshua <joshuajcb@btinternet.com>
2021-10-05 17:19:54 +0100

    Quality of life update + Changed Sprites
    + Added option to leave the game whenever
    + Pressing ESC twice leaves the game
    + Updated the company logo
    + Updated CV to a false one
    + Changed window material based on first day actions

---
+++ Assets/Scripts/Destruction/Window.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Window : MonoBehaviour
+{
+    [SerializeField] private Material[] mat;
+
+    private void Start()
+    {
+        if (Manager.Instance.killed[0])
+            GetComponent<Renderer>().material = mat[0];
+
+        else
+            GetComponent<Renderer>().material = mat[1];
+    }
+
+}
---
+++ Assets/Scripts/Player/Pause.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Pause : MonoBehaviour
+{
+    private bool                            pressed             = false;
+
+    private float                           count_timer         = 0.0f;
+    [SerializeField] private float          timer               = 0.0f;
+    [SerializeField] private TMP_Text       text;
+
+    private void Update()
+    {
+        Timer();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pressed)
+                pressed = true;
+
+            else
+                Manager.Instance.QuitGame();
+        }
+    }
+
+    private void Timer()
+    {
+        if (pressed)
+        {
+            text.text = "Press ESC again to quit";
+            count_timer += Time.deltaTime;
+
+            if (count_timer >= timer)
+            {
+                pressed     = false;
+                count_timer = 0.0f;
+                text.text   = "";
+            }
+        }
+    }
+}
